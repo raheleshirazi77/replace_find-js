b4be9ba2c8787b6d77d60cd1f08108e48c7823f2
rhele shirazi <124025212+raheleshirazi77@users.noreply.github.com>
2024-01-10 15:20:17 +0330

    uppdate js and html
    update page js
    create page js
    linked page js in html

---
+++ script.js
@@ -0,0 +1,6 @@
+// replace🤓 :
+let text = "I Am Rahele Sherozi"; // Define variable to create text
+function repText() { // Create a function to perform the operation of replacing the desired text
+    document.write("replaced: ",text.replace("Sherozi", "Shirazi")); // Display text on the screen
+}
+repText() //Call the replace function And Tamam Tamam😁
